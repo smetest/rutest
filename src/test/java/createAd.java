7ee513247f4de9fea9e6773655845c7c33ea3f17
tod-eug <32629401+tod-eug@users.noreply.github.com>
2018-09-04 19:54:36 +0600

    feat/testAd
    Test Ad

---
+++ src/test/java/createAd.java
@@ -0,0 +1,165 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.Test;
+import pages.AutoRuAddAdPage;
+import pages.AutoRuMainPage;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static org.openqa.selenium.Keys.DOWN;
+
+public class createAd {
+    @Test
+            public void createAdTest() {
+
+        AutoRuAddAdPage autoRuAddAdPage = new AutoRuAddAdPage();
+        AutoRuMainPage autoRuMainPage = new AutoRuMainPage();
+
+
+        //Тестовые данные
+        String url = "http://www.auto.ru";
+        String pathToFile1 = "src/test/resources/front.jpg";
+        String pathToFile2 = "src/test/resources/rear.jpeg";
+
+        String mileage = "55000";
+        String city = "Омск";
+        String address = "ул. Андрианова д.10";
+        String phone = "9058662233";
+        String name = "Алексей Владимирович";
+        String email = "test@test.ru";
+        String price = "2000000";
+
+        String regNumber = "с999сс";
+        String region = "55";
+        String vin = "WAUYP64B01N141245";
+        String sts = "78ОТ 757744";
+        String description = "Машина огонь!";
+
+
+        //Открываем сайт
+        open(url);
+
+
+        //Нажимаем кнопку "Продать"
+        autoRuMainPage.$addAdButton.click();
+
+
+        //Выбираем марку, модель и год выпуска
+        autoRuAddAdPage.$company.click();
+        autoRuAddAdPage.$model.click();
+        autoRuAddAdPage.$year.click();
+
+
+        //Ждем когда появится форма и выбираем правый руль и поколение
+        autoRuAddAdPage.$wheelOnTheRight.waitUntil(visible, 30000);
+        autoRuAddAdPage.$wheelOnTheRight.click();
+        autoRuAddAdPage.$generation.click();
+
+
+        //Кликаем по кнопке "Бензин" и устанавливаем чекбокс ГБО
+        autoRuAddAdPage.$benzinEngine.click();
+        autoRuAddAdPage.$gbo.click();
+
+
+        //Кликаем по кнопкам "Полный" привод, "Автоматическая" КПП и модификация в 407 лс
+        autoRuAddAdPage.$allWheelDrive.click();
+        autoRuAddAdPage.$automaticGearbox.click();
+        autoRuAddAdPage.$modification407Power.click();
+
+
+
+        //Делаем поле для загрузки файлов доступным
+        executeJavaScript("$('.photos-add__input').attr('style', 'display: block;');");
+
+
+        //Выбираем первый файл, ждем загрузки и подтверждаем. Тоже самое для второго файла
+        autoRuAddAdPage.$photo.uploadFile(new File(pathToFile1));
+        autoRuAddAdPage.$modalDoneButton.waitUntil(visible, 30000);
+        autoRuAddAdPage.$modalDoneButton.click();
+        autoRuAddAdPage.$photo.uploadFile(new File(pathToFile2));
+        autoRuAddAdPage.$modalDoneButton.waitUntil(visible, 30000);
+        autoRuAddAdPage.$modalDoneButton.click();
+
+
+        //Кликаем на синий цвет, вводим пробег, устанавливаем чекбоксы битый и нерастаможенный
+        autoRuAddAdPage.$colorBlue.click();
+        autoRuAddAdPage.$run.sendKeys(mileage);
+        autoRuAddAdPage.$crashed.click();
+        autoRuAddAdPage.$custom.click();
+
+
+        //Вводим город, адрес, номер телефона, имя и email
+        autoRuAddAdPage.$city.sendKeys(city);
+        autoRuAddAdPage.$address.sendKeys(address);
+        autoRuAddAdPage.$phone.sendKeys(phone);
+        autoRuAddAdPage.$name.sendKeys(name);
+        autoRuAddAdPage.$email.sendKeys(email);
+
+
+        //Вводим цену и указываем возможность обмена
+        autoRuAddAdPage.$price.sendKeys(price);
+        autoRuAddAdPage.$exchange.click();
+
+
+        //Указываем дубликат ПТС, второго владельца, выбираем время покупки авто и устанавливаем чекбокс гарантии
+        autoRuAddAdPage.$duplicate.click();
+        autoRuAddAdPage.$secondOwner.click();
+
+
+        //Выбираем год покупки авто
+        autoRuAddAdPage.$buyYear.click();
+
+        int i;
+        for (i=0; i<8; i++) {
+            autoRuAddAdPage.$buyYear.sendKeys(DOWN);
+        }
+
+        autoRuAddAdPage.$buyYear.pressEnter();
+
+
+        //Выбираем месяц покупки авто
+        autoRuAddAdPage.$buyMonth.click();
+
+        for (i=0; i<7; i++) {
+            autoRuAddAdPage.$buyMonth.sendKeys(DOWN);
+        }
+
+        autoRuAddAdPage.$buyMonth.pressEnter();
+
+
+        //Вводим ГРЗ, VIN и СТС
+        autoRuAddAdPage.$regNumber.sendKeys(regNumber);
+        autoRuAddAdPage.$region.sendKeys(region);
+        autoRuAddAdPage.$vin.sendKeys(vin);
+        autoRuAddAdPage.$sts.sendKeys(sts);
+
+
+        //Указываем максимальную комплектацию и выбираем размер колес
+        autoRuAddAdPage.$farsh.click();
+        autoRuAddAdPage.$wheels.click();
+
+        for (i=0; i<6; i++) {
+            autoRuAddAdPage.$wheels.sendKeys(DOWN);
+        }
+
+        autoRuAddAdPage.$wheels.pressEnter();
+
+
+        //Вводим описание
+        autoRuAddAdPage.$description.sendKeys(description);
+
+
+        //Выбираем опцию "Обычная продажа"
+        autoRuAddAdPage.$usualSale.click();
+
+
+        //И не нажимаем кнопку "Разместить"
+        //$submitForm.click();
+
+
+        //Подождем 15 секунд чтобы убедиться, что все заполнено
+        Selenide.sleep(15000);
+    }
+
+}
---
+++ src/test/java/pages/AutoRuAddAdPage.java
@@ -0,0 +1,97 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class AutoRuAddAdPage {
+
+    //Шаг 1
+    public SelenideElement $company = $$("img").findBy(attribute("title", "BMW")).parent();
+
+
+    //Шаг 2
+    public SelenideElement $model = $$("div").findBy(Condition.text("X5"));
+
+
+    //Шаг 3
+    public SelenideElement $yearsBlock = $(".years-list__controls");
+    public SelenideElement $year = $yearsBlock.$$("input").findBy(attribute("value", "2010")).parent();
+
+    public SelenideElement $wheelOnTheRight = $$("input").findBy(Condition.name("wheel")).parent().parent();
+
+    public SelenideElement $generationsForm = $(".generations-list");
+    public SelenideElement $generation = $generationsForm.$$("button").first();
+
+    public SelenideElement $enginesForm = $(".engines");
+    public SelenideElement $benzinEngine = $enginesForm.$$("input").findBy(attribute("value", "GASOLINE")).parent();
+    public SelenideElement $gbo = $enginesForm.$$("input").findBy(Condition.name("gbo")).parent().parent();
+
+    public SelenideElement $drivesForm = $(".drives");
+    public SelenideElement $allWheelDrive = $drivesForm.$$("button").first().parent();
+
+    public SelenideElement $gearboxesForm = $(".gearboxes");
+    public SelenideElement $automaticGearbox = $gearboxesForm.$$("input").findBy(attribute("value", "AUTOMATIC")).parent();
+
+    public SelenideElement $modificationsForm = $(".modifications-list");
+    public SelenideElement $modification407Power = $modificationsForm.$$("input").findBy(attribute("value", "6275113")).parent();
+
+
+    //Шаг 4
+    public SelenideElement $photosListForm = $(".photos-list");
+    public SelenideElement $photo = $photosListForm.$(".photos-add__input");
+    public SelenideElement $modalFormUpload = $("div.inner.inner_bottom-fixed.clearfix.i-bem");
+    public SelenideElement $modalDoneButton = $modalFormUpload.$(".photos-list__done-button");
+
+    public SelenideElement $colorForm = $(".color-selector");
+    public SelenideElement $colorBlue = $colorForm.$$("input").findBy(attribute("value", "0433FF")).parent().parent();
+
+    public SelenideElement $runForm = $(".run");
+    public SelenideElement $run = $runForm.$$("input").findBy(Condition.name("run"));
+    public SelenideElement $crashed = $runForm.$$("input").findBy(Condition.name("state")).parent().parent();
+    public SelenideElement $custom = $runForm.$$("input").findBy(Condition.name("custom")).parent().parent();
+
+    public SelenideElement $addressForm = $(".address");
+    public SelenideElement $city = $addressForm.$(".input__control");
+    public SelenideElement $address = $addressForm.$$("input").findBy(Condition.name("address"));
+
+    public SelenideElement $phoneForm = $(".phone-item");
+    public SelenideElement $phone = $phoneForm.$(".input__control");
+
+    public SelenideElement $contactsForm = $(".contacts");
+    public SelenideElement $name = $contactsForm.$$("input").findBy(Condition.name("username"));
+    public SelenideElement $email = $contactsForm.$$("input").findBy(Condition.name("useremail"));
+
+    public SelenideElement $priceForm = $(".price");
+    public SelenideElement $price = $priceForm.$$("input").findBy(Condition.name("price"));
+    public SelenideElement $exchange = $priceForm.$$("input").findBy(Condition.name("exchange")).parent().parent();
+
+    public SelenideElement $ptsForm = $(".pts");
+    public SelenideElement $duplicate = $ptsForm.$$("input").findBy(attribute("value", "2")).parent();
+
+    public SelenideElement $ownersForm = $(".owners");
+    public SelenideElement $secondOwner = $ownersForm.$$("input").findBy(attribute("value", "2")).parent();
+
+    public SelenideElement $buyTimeForm = $(".purchase-container");
+    public SelenideElement $buyYear = $buyTimeForm.$$(".section-cell__half").first().$("span.icon.select__tick");
+    public SelenideElement $buyMonth = $buyTimeForm.$$(".section-cell__half").get(1).$("span.icon.select__tick");
+
+    public SelenideElement $autoDataForm = $(".autocode");
+    public SelenideElement $regNumber = $autoDataForm.$(".gos-number__series-number").$("input");
+    public SelenideElement $region = $autoDataForm.$(".gos-number__region").$("input");
+    public SelenideElement $vin = $autoDataForm.$$("input").findBy(Condition.name("vin"));
+    public SelenideElement $sts = $autoDataForm.$$("input").findBy(Condition.name("sts"));
+
+    public SelenideElement $extrasForm = $(".extras");
+    public SelenideElement $farsh = $extrasForm.$$("input").findBy(attribute("value", "6275090_7151419_6275113")).parent();
+    public SelenideElement $wheels = $extrasForm.$("span.icon.select__tick");
+    public SelenideElement $description = $extrasForm.$("textarea");
+
+    public SelenideElement $publishForm = $(".vas-container");
+    public SelenideElement $usualSale = $publishForm.$(".vas-pack__free-title");
+
+    public SelenideElement $submitForm = $(".vas-submit__submit-text");
+}
